58483c59554e91adad2b812f93f62bf6905fd6d8
Anand Desai <desaiaj@rose-hulman.edu>
2023-06-03 19:46:54 -0400

    Updates

--- index.d3cbf8ef.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(e){e.target.checked}document.addEventListener("DOMContentLoaded",(function(){document.getElementById("toggle-switch").addEventListener("change",e)}))}();
-//# sourceMappingURL=index.d3cbf8ef.js.map
