0d90473006ece7f59a59a0c79b2b27f18c4ed3de
Anand Desai <desaiaj@rose-hulman.edu>
2023-07-26 03:33:01 -0400

    changed tailwind custom theme. css broken in the interim

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,43 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#0046ff",
-        "on-primary": "#FFFFFF",
-        "primary-tint-10": "#ccdaff",
-        "on-primary-tint-10": "#00154c",
-        "primary-tint-30": "#99b5ff",
-        "on-primary-tint-30": "#00154c",
-        "primary-shade-70": "#0038cc",
-        "on-primary-shade-70": "#EBF1FF",
-        deemphasized: "#444b6a",
-        "primary-dark": "#7096ff",
-        "on-primary-dark": "#000A24",
-        "primary-dark-shade-90": "#000e33",
-        "on-primary-dark-shade-90": "#759AFF",
-        "primary-dark-shade-70": "#002380",
-        "on-primary-dark-shade-70": "#A8BFFF",
-        "primary-dark-tint-30": "#8FADFF",
-        "on-primary-dark-tint-30": "#00185C",
-        "deemphasized-dark": "#444b6a",
-        "hero-img": "#25293B",
-      },
-      fontFamily: {
-        sans: [
-          "Poppins",
-          "Lato",
-          "Noto Sans",
-          "Open Sans",
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
+    content: ["./src/**/*.{html,js,ts}"],
+    theme: {
+        extend: {
+            colors: {
+                deemphasized: "#444b6a",
+                "hero-example-img": "#25293C",
+                "primary-100": "#0046ff",
+                "primary-200": "#4d59ff",
+                "primary-300": "#6e6cff",
+                "primary-400": "#8980ff",
+                "primary-500": "#a094ff",
+                "primary-600": "#b5a9ff",
+                /** CSS DARK THEME SURFACE COLORS */
+                "surface-100": "#000e33",
+                "surface-200": "#1c2547",
+                "surface-300": "#363c5c",
+                "surface-400": "#515472",
+                "surface-500": "#6c6e88",
+                "surface-600": "#87899f",
+                /** CSS DARK THEME BLENDED SURFACE COLORS */
+                "surface-blend-100": "#001545",
+                "surface-blend-200": "#222a58",
+                "surface-blend-300": "#3d406b",
+                "surface-blend-400": "#57597f",
+                "surface-blend-500": "#717294",
+                "surface-blend-600": "#8c8ca8",
+            },
+            fontFamily: {
+                sans: [
+                    "Poppins",
+                    "Lato",
+                    "Noto Sans",
+                    "Open Sans",
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
